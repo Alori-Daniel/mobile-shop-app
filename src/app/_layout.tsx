2dd2a092ade80a7e679ad794123cec6619a4cb52
Alori Daniel <aloridaniel123@gmail.com>
2026-05-19 12:46:14 +0100

    chore: added app logo

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,19 +1,14 @@
-import { router, Stack } from "expo-router";
-import { useEffect } from "react";
+import { Stack } from "expo-router";
 import { StyleSheet } from "react-native";
 
 const _layout = () => {
-  useEffect(() => {
-    setTimeout(() => {
-      //@ts-ignore
-      router.push("(tabs)/home");
-    }, 2000);
-  }, []);
-
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="(tabs)"
+        options={{ headerShown: false, animation: "fade" }}
+      />
     </Stack>
   );
 };
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,38 @@
-import { StyleSheet, Text, View } from "react-native";
-
+import { router } from "expo-router";
+import { useEffect } from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
 const index = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      //@ts-ignore
+      router.push("(tabs)/home");
+    }, 2000);
+  }, []);
   return (
-    <View>
-      <Text>index</Text>
+    <View style={styles.container}>
+      <Image
+        source={require("../../assets/images/shopLogo.png")}
+        style={styles.image}
+      />
+      <Text style={styles.text}>Shopping Made Easy</Text>
     </View>
   );
 };
 
 export default index;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+  text: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+});
