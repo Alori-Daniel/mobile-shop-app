9563421609e9de75c54722ee461c9bdae9e6792d
Alori Daniel <aloridaniel123@gmail.com>
2026-05-19 13:59:18 +0100

    feat: implemented folder structure for screens

---
+++ src/app/(screens)/orderConfirmation.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const orderConfirmation = () => {
+  return (
+    <View>
+      <Text>orderConfirmation</Text>
+    </View>
+  );
+};
+
+export default orderConfirmation;
+
+const styles = StyleSheet.create({});
---
+++ src/app/(screens)/product/[id].tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const productId = () => {
+  return (
+    <View>
+      <Text>productId</Text>
+    </View>
+  );
+};
+
+export default productId;
+
+const styles = StyleSheet.create({});
